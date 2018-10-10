e7239d667bb688da3b54088c60e756cf2969b1e8
Magdalena <magda.dabrowska1026@gmail.com>
2018-10-10 22:39:32 +0200

    14.4v2

--- src/main/java/com/crud/tasks/KodillaCourseApplication.java
+++ src/main/java/com/crud/tasks/KodillaCourseApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TasksApplication {
+public class KodillaCourseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TasksApplication.class, args);
+        SpringApplication.run(KodillaCourseApplication.class, args);
     }
 }
--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -16,19 +16,19 @@ public class TaskController {
     public List<TaskDto> getTasks(){
         return new ArrayList<>();
     }
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long taskId){
         return new TaskDto(1L, "test title", "test_content");
     }
-    @RequestMapping(method = RequestMethod.GET, value = "deleteTask")
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask(Long taskId){
 
     }
-    @RequestMapping(method = RequestMethod.GET, value = "updateTask")
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
     public TaskDto updateTask(TaskDto taskDto){
         return new TaskDto(1L, "Edit test title", "Test content");
     }
-    @RequestMapping(method = RequestMethod.GET, value = "createTask")
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
     public void createTask(TaskDto taskDto){
 
     }
